8f13fbca196b939f4cb31c1756a6f63080c1943f
Igor <igor.tkachenko@trustonic.com>
2020-11-09 18:06:03 +0200

    UI

---
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
